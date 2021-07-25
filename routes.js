8b34812c376c62e3b0e52752623cec7adecebff4
ngoding22 <69552402+ngoding22@users.noreply.github.com>
2021-07-25 16:45:02 +0700

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,33 @@
+const {
+  // eslint-disable-next-line max-len
+  addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookid}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookid}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookid}',
+    handler: deleteBookByIdHandler,
+  },
+];
+module.exports = routes;
